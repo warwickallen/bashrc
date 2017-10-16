062c6b75743e26451bcf37b0df314402506d55e6
Warwick Allen <warwick.allen@clearpoint.co.nz>
2017-10-16 14:39:58 +1300

    Load nvm
    From the bashrc, load Node Version Manager (nvm) and its bash-completion
    file.

--- bashrc.sh
+++ bashrc.sh
@@ -256,3 +256,7 @@ export EDITOR=vim
 export PYTHON=$(which python)
 
 [[ -f ~/.bash_local_post ]] && source ~/.bash_local_post
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
