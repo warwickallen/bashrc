1e6badc0322fb17ad3852771d739acc99dc33406
Warwick Allen <warwick.allen@clearpoint.co.nz>
2018-05-24 11:40:00 +1200

    Modify "run-as" function to use xpra
    "run-as --xpra user command ..." launches and connects to an xpra
    session running "command".

--- bash_functions/run-as.sh
+++ bash_functions/run-as.sh
@@ -7,13 +7,56 @@
 
 function run-as
 {
-  user=$1
+  # Parse parameters.
+  no_more_options=false
+  use_xpra=false
+  user=""
+  cmd=""
+  params=""
+  while [ $# -gt 0 ]
+  do
+    if [ "$1" = "--" ]
+    then
+      no_more_options=true
+    elif [ "$no_more_options" = true ] || [ "${1:0:1}" != "-" ] && [ "${1:0:1}" != "+" ]
+    then
+      if [ -z "$user" ]
+      then
+        user="$1"
+      elif [ -z "$cmd" ]
+      then
+        cmd="$1"
+        no_more_options=true
+      else
+        params="$params $1"
+      fi
+    elif [ "$1" = "-x" ] || [ "$1" = "--xpra" ]
+    then
+      use_xpra=true
+    elif [ "$1" = "+x" ] || [ "$1" = "--no-xpra" ]
+    then
+      use_xpra=false
+    fi
+    shift
+  done
+
   logdir="$HOME/.run-as"
-  mkdir -p "$logdir"
-  shift
+  mkdir -p "${logdir}"
   (
-    set -x
-    logname="${logdir}/${user}.$(sed 's,/,~,g'<<<"$1").log"
-    ssh -Xfi"$HOME/.ssh/$user.rsa" "$user@localhost" "$@" >>"${logname}" 2>&1
+    logname="${logdir}/${user}.$(sed 's,/,~,g'<<<"${cmd}").log"
+    function _ssh
+    {
+      set -x
+      ssh -i"$HOME/.ssh/${user}.rsa" "${user}@localhost" "$@" >>"${logname}" 2>&1
+    }
+    if [ $use_xpra = true ]
+    then
+      tcp_port=$(( $$ % 0x3000 + 0x1000 ))
+      _ssh xpra start --bind-tcp localhost:${tcp_port} :${tcp_port} --start "'${cmd} ${params}'"
+      sleep 1
+      nohup xpra attach tcp:localhost:${tcp_port} >>"${logname}" 2>&1 &
+    else
+      _ssh -Xf ${cmd} ${params}
+    fi
   )
 }
